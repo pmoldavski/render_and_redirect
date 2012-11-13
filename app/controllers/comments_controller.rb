d3196bdb114d567a2ec7d6530d7a9a40e2ee91d8
Apprentice <apprentice@dbc25.local>
2012-11-13 12:39:20 -0800

    Initial commit

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,7 +10,7 @@ class CommentsController < ApplicationController
         format.html { redirect_to @comment.article, notice: 'Comment was successfully created.' }
         format.json { render json: @comment, status: :created, location: @comment }
       else
-        format.html { redirect_to @comment.article }
+        format.html { redirect_to @comment.article, notice: 'A comment must not be empty.' }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
       end
     end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,7 @@ class Comment < ActiveRecord::Base
   belongs_to :article
 
   attr_accessible :body
+
+  validates :body, :presence => true
+
 end
