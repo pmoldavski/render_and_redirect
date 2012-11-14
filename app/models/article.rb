0c866c25b01cacefbaf11d2ff51cb29de4669da1
Apprentice <apprentice@dbc25.local>
2012-11-13 16:38:58 -0800

    Solved exercise

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,12 @@
+require 'text_validations'
+
 class Article < ActiveRecord::Base
+  include TextValidations
   has_many :comments, dependent: :destroy
 
   attr_accessible :body, :title
+
+  validate :word_censor
+  validates :body,  :presence => true 
+  validates :title, :presence => true
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,12 @@
+require 'text_validations'
+
 class Comment < ActiveRecord::Base
+  include TextValidations
   belongs_to :article
+  validates_associated :article 
+  validate :word_censor
 
   attr_accessible :body
 
-  validates :body, :presence => true
-
+  validates :body, :presence => true, :length => { :maximum => 250 } 
 end
